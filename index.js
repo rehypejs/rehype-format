8df91f48a1164092172a2ed85421eaa1d5dfe510
Titus Wormer <tituswormer@gmail.com>
2018-07-18 16:44:43 -0400

    Refactor code-style

--- index.js
+++ index.js
@@ -1,140 +1,143 @@
-'use strict';
+'use strict'
 
-var minify = require('rehype-minify-whitespace')({newlines: true});
-var phrasing = require('hast-util-phrasing');
-var sensitive = require('html-whitespace-sensitive-tag-names');
-var repeat = require('repeat-string');
-var visit = require('unist-util-visit-parents');
+var minify = require('rehype-minify-whitespace')({newlines: true})
+var phrasing = require('hast-util-phrasing')
+var sensitive = require('html-whitespace-sensitive-tag-names')
+var repeat = require('repeat-string')
+var visit = require('unist-util-visit-parents')
 
-module.exports = format;
+module.exports = format
 
 /* Constants. */
-var double = '\n\n';
-var single = '\n';
-var re = /\n/g;
+var double = '\n\n'
+var single = '\n'
+var re = /\n/g
 
 /* Format white-space. */
 function format(options) {
-  var settings = options || {};
-  var indent = settings.indent || 2;
-  var indentInitial = settings.indentInitial;
-  var blanks = settings.blanks || [];
+  var settings = options || {}
+  var indent = settings.indent || 2
+  var indentInitial = settings.indentInitial
+  var blanks = settings.blanks || []
 
   if (typeof indent === 'number') {
-    indent = repeat(' ', indent);
+    indent = repeat(' ', indent)
   }
 
   /* Default to indenting the initial level. */
   if (indentInitial === null || indentInitial === undefined) {
-    indentInitial = true;
+    indentInitial = true
   }
 
-  return transform;
+  return transform
 
   function transform(tree) {
-    var root = minify(tree);
-    var head = false;
+    var root = minify(tree)
+    var head = false
 
-    visit(root, visitor);
+    visit(root, visitor)
 
-    return root;
+    return root
 
     function visitor(node, parents) {
-      var children = node.children || [];
-      var length = children.length;
-      var level = parents.length;
-      var index = -1;
-      var result;
-      var prev;
-      var child;
-      var newline;
+      var children = node.children || []
+      var length = children.length
+      var level = parents.length
+      var index = -1
+      var result
+      var prev
+      var child
+      var newline
 
       if (node.type === 'element' && node.tagName === 'head') {
-        head = true;
+        head = true
       }
 
       if (head && node.type === 'element' && node.tagName === 'body') {
-        head = false;
+        head = false
       }
 
       /* Don’t indent content of whitespace-sensitive nodes / inlines. */
       if (!length || !padding(node, head) || ignore(parents.concat(node))) {
-        return;
+        return
       }
 
       if (!indentInitial) {
-        level--;
+        level--
       }
 
       /* Indent newlines in `text`. */
       while (++index < length) {
-        child = children[index];
+        child = children[index]
 
         if (child.type === 'text') {
           if (child.value.indexOf('\n') !== -1) {
-            newline = true;
+            newline = true
           }
 
-          child.value = child.value.replace(re, '$&' + repeat(indent, level));
+          child.value = child.value.replace(re, '$&' + repeat(indent, level))
         }
       }
 
-      result = [];
-      index = -1;
+      result = []
+      index = -1
 
-      node.children = result;
+      node.children = result
 
       while (++index < length) {
-        child = children[index];
+        child = children[index]
 
         if (padding(child, head) || (newline && index === 0)) {
           result.push({
             type: 'text',
-            value: ((prev && blank(prev) && blank(child)) ? double : single) +
+            value:
+              (prev && blank(prev) && blank(child) ? double : single) +
               repeat(indent, level)
-          });
+          })
         }
 
-        prev = child;
-        result.push(child);
+        prev = child
+        result.push(child)
       }
 
       if (newline || padding(prev, head)) {
         result.push({
           type: 'text',
           value: single + repeat(indent, level - 1)
-        });
+        })
       }
     }
   }
 
   function blank(node) {
-    return node.type === 'element' &&
+    return (
+      node.type === 'element' &&
       blanks.length !== 0 &&
-      blanks.indexOf(node.tagName) !== -1;
+      blanks.indexOf(node.tagName) !== -1
+    )
   }
 }
 
 function padding(node, head) {
   if (node.type === 'root') {
-    return true;
+    return true
   }
 
   if (node.type === 'element') {
-    return node.tagName === 'script' || !phrasing(node) || head;
+    return node.tagName === 'script' || !phrasing(node) || head
   }
 
-  return false;
+  return false
 }
 
 function ignore(nodes) {
-  var index = nodes.length;
+  var index = nodes.length
 
   while (index--) {
     if (sensitive.indexOf(nodes[index].tagName) !== -1) {
-      return true;
+      return true
     }
   }
 
-  return false;
+  return false
 }
