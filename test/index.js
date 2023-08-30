f7de91980961698011f09e9b2a9803551230cf9f
Titus Wormer <tituswormer@gmail.com>
2023-08-30 18:02:25 +0200

    Change to use `exports`

--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 /**
- * @typedef {import('../index.js').Options} Options
+ * @typedef {import('rehype-format').Options} Options
  */
 
 import assert from 'node:assert/strict'
@@ -7,12 +7,12 @@ import fs from 'node:fs/promises'
 import test from 'node:test'
 import {isHidden} from 'is-hidden'
 import {rehype} from 'rehype'
+import rehypeFormat from 'rehype-format'
 import {read} from 'to-vfile'
-import rehypeFormat from '../index.js'
 
 test('rehype-format', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('../index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('rehype-format')).sort(), [
       'default'
     ])
   })
